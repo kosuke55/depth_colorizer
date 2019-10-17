d3bf48e48fd1e9494b0f0505dac26d87b277fada
kosuke55 <kosuke.tnp@gmail.com>
2019-10-18 02:56:56 +0900

    update readme and code

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # depth_colorizer
 
 Colorize depth image according to the value.  
+You can change the distance range dynamically  
+
 <div align = center>
-<img src="img/depth_raw.png" width="300">
-<img src="img/visualized_depth.jpg" width="300">
+<img src="img/depth_raw.png" width="250">
+<img src="img/colorized_depth_0.png" width="250">
+<img src="img/colorized_depth_1.png" width="250">
 <div>
--- scripts/depthcolorizer.py
+++ scripts/depthcolorizer.py
@@ -15,7 +15,7 @@ class Depthcolorizer():
         self.srv = Server(DynamicParamsConfig,
                           self.dynamic_reconfigure_callback)
         self.bridge = CvBridge()
-        self.pub = rospy.Publisher("/visualized_depth", Image, queue_size=1)
+        self.pub = rospy.Publisher("/colorized_depth", Image, queue_size=1)
         self.INPUT_IMAGE = rospy.get_param(
             '~input_image', "/head_mount_kinect/hd/image_depth_rect_desktop")
         self.max_distance = rospy.get_param(
@@ -31,16 +31,16 @@ class Depthcolorizer():
 
     def callback(self, msg):
         img = self.bridge.imgmsg_to_cv2(msg, "16UC1")
-        visualized_depth = np.full((img.shape[0], img.shape[1], 3), 255,
+        colorized_depth = np.full((img.shape[0], img.shape[1], 3), 255,
                                    dtype=np.uint8)
-        visualized_depth[:, :, 0] \
+        colorized_depth[:, :, 0] \
             = (img - self.min_distance) * 180. \
             / (self.max_distance - self.min_distance)
-        visualized_depth[img < self.min_distance] = 0
-        visualized_depth[img > self.max_distance] = 0
-        visualized_depth = cv2.cvtColor(visualized_depth.astype(np.uint8),
+        colorized_depth[img < self.min_distance] = 0
+        colorized_depth[img > self.max_distance] = 0
+        colorized_depth = cv2.cvtColor(colorized_depth.astype(np.uint8),
                                         cv2.COLOR_HSV2RGB)
-        msg_out = self.bridge.cv2_to_imgmsg(visualized_depth, "rgb8")
+        msg_out = self.bridge.cv2_to_imgmsg(colorized_depth, "rgb8")
         msg_out.header = msg.header
         self.pub.publish(msg_out)
 
